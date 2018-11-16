4b307b80926f344bc809dbae4cbaf4bd27c438b5
root <ngomfa@gmail.com>
2018-11-16 15:54:15 +0100

    exo7

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Partie1_Exo7</title>
+</head>
+<body>
+
+    <?php
+    //1-Créer trois variables lastname , firstname et age et les initialiser avec les valeurs de votre choix. Attention age est de type entier.
+    //2-Afficher : "Bonjour" + lastname + firstname + ",tu as" + age + "ans".
+    $lastname = "John";
+    $firstname = "DOE";
+    $age = 17;
+      echo "Bonjour " . $lastname . " " . $firstname . " tu as " . $age . " ans" ;
+  ?>
+</body>
+</html>
